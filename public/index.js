22c0b23e4cca9a3de695e3309b5fe558dad1f7f5
Sorravit <kanomcake1928@gmail.com>
2020-09-21 23:45:37 +0700

    Add: Get posts

--- public/index.js
+++ public/index.js
@@ -1,12 +1,40 @@
-$(function() {
+$(async function() {
 
     const apiUrl = "https://instaapi-born.herokuapp.com/api/posts";
 
-    $.get(apiUrl, function(data, status) {
+    try {
+        const response = await axios.get(apiUrl)
+        console.log(response);
+        $("#posts").empty();
+        for (const postid in response.data.data) {
+            const post = response.data.data[postid];
+            let row = `<tr>
+                    <td scope="row">${post.shortcode}</td>
+                    <td>${post.full_name}</td>
+                </tr> `;
+            $("#posts").append(row);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
-        console.log(data);
 
-    });
+
+    // $.get(apiUrl, function(data, status) {
+
+    //     console.log(data);
+
+    //     $("#posts").empty();
+    //     for (const postid in data.data) {
+    //         const post = data.data[postid];
+    //         let row = `<tr>
+    //                         <td scope="row">${post.shortcode}</td>
+    //                         <td>${post.full_name}</td>
+    //                     </tr> `;
+    //         $("#posts").append(row);
+    //     }
+
+    // });
 
 
 })
